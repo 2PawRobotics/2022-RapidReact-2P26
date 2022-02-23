901c7dcab1f1fd891c5e4677b2cfa70c77fd39eb
2PawRobotics <hsrobotics@ppps.org>
2022-02-23 18:34:48 -0500

    Update ClimbSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -8,13 +8,21 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
 
 public class ClimbSubsystem extends SubsystemBase {
 
+  //Declare Hardware Components
   private final CANSparkMax actuatorMotor = new CANSparkMax(Constants.actuatorport, MotorType.kBrushed);
   private final AnalogPotentiometer pot = new AnalogPotentiometer(0, 500, 0);
+
+  private final DoubleSolenoid dSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+  private final DoubleSolenoid dSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 3, 4);
 
   @Override
   public void periodic() {
@@ -26,5 +34,16 @@ public class ClimbSubsystem extends SubsystemBase {
 
     System.out.println(pot.get());
 
+  }
+
+  public void SolenoidControl(){
+    if(RobotContainer.ButtonPanel.getRawButtonPressed(Constants.ButtonPort5)){
+      dSolenoid1.set(Value.kForward);
+      dSolenoid2.set(Value.kForward);
+    }
+    if(RobotContainer.ButtonPanel.getRawButtonPressed(Constants.ButtonPort6)){
+      dSolenoid1.set(Value.kReverse);
+      dSolenoid2.set(Value.kReverse);
+    }
   }
 }
